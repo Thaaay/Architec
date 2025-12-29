af75550c6c90a680b5c7145c8ea39163e35b389c
Thaaay <thaynarapozzergimenes1@gmail.com>
2025-12-29 16:48:48 +0000

    fix: forcing lazy load and single database for free tier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,32 +1,22 @@
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
-  config.eager_load = true
+  # DESATIVE ISSO PARA DEBUGAR E SUBIR NO PLANO FREE
+  config.eager_load = false
 
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
-  # Turn on fragment caching in view templates.
-  config.action_controller.perform_caching = true
-
-  # Cache assets for far-future expiry since they are all digest stamped.
-  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  #config.active_storage.service = :local
-
+  # Configuração do Cloudinary
   config.active_storage.service = :cloudinary
 
-  
+  # Host do seu site (MUITO IMPORTANTE)
+  config.action_controller.default_url_options = { host: "architec-xu6c.onrender.com" }
+
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
