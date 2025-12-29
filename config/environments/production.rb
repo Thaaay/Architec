f93e5888b9a8803b58aa817d81a00288fccd533b
Thaaay <thaynarapozzergimenes1@gmail.com>
2025-12-29 18:05:48 +0000

    fix: remove auto migration to fix docker build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,9 +86,6 @@ Rails.application.configure do
 
   config.action_controller.default_url_options = { host: "architec-xu6c.onrender.com" }
 
-# Isso força a migração ao iniciar o servidor
-  config.after_initialize do
-    ActiveRecord::Tasks::DatabaseTasks.migrate
-  end
+
 end
 
