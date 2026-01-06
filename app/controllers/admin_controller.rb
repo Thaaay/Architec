ff2adbb1fd6823b487058b762a0af83577a61b05
Thaaay <thaynarapozzergimenes1@gmail.com>
2026-01-06 22:29:07 +0000

    fix: carrossel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,16 @@
 class AdminController < ApplicationController
   before_action :authenticate_admin!
 
+  def dashboard
+    
+    @projects = Project.all 
+  end
+
   private
 
   def authenticate_admin!
-   
     unless session[:admin_id]
-      redirect_to login_path, alert: "Access Denied. Identity required."
+      redirect_to login_path, alert: "Access Denied."
     end
   end
 end
