90b8e928a45254f3c029561b403f0021827287d1
Thaaay <thaynarapozzergimenes1@gmail.com>
2026-01-07 09:12:26 +0000

    fix: auth1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
-class ApplicationController < ActionController::Base
-  def authenticate_admin!
-   
-    if session[:admin_id].nil?
-      redirect_to login_path, alert: "Acesso restrito."
-    end
+def authenticate_admin!
+
+  unless session[:admin_id] == true
+    redirect_to login_path, alert: "Acesso Negado."
   end
 end
