663b3f9d7feb089b03a8d253cf9bff3ed6c9bac1
Thaaay <thaynarapozzergimenes1@gmail.com>
2026-01-07 09:18:07 +0000

    fix: restore missing ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
-def authenticate_admin!
-
-  unless session[:admin_id] == true
-    redirect_to login_path, alert: "Acesso Negado."
+class ApplicationController < ActionController::Base
+  
+  def authenticate_admin!
+    unless session[:admin_id] == true
+      redirect_to login_path, alert: "Acesso restrito."
+    end
   end
 end
