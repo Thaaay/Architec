fdcb4a92531c4e7b8e5f96657b943057dd423dab
Thaaay <thaynarapozzergimenes1@gmail.com>
2026-05-21 20:13:44 -0300

    Fix: force production database to use DATABASE_URL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-
+ config.hosts.clear
 end
